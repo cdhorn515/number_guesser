443e57258d30bb62363228f2e97e9445b7644af4
Christina Cornell <cdhorn515@gmail.com>
2017-10-09 22:26:28 -0400

    number guessing game

---
+++ number_guesser.py
@@ -0,0 +1,27 @@
+
+import random
+
+guessesLeft = 5
+guesses = 0
+
+numberToGuess = random.randint(1, 10)
+
+while guessesLeft > 0:
+    userGuess = int(input("Please enter a number between 1 and 100: "))
+    guesses += 1
+    guessesLeft -= 1
+    if userGuess == numberToGuess:
+        print(f"Hooray! You guessed the number in {guesses} guesses!")
+        answer = input("Do you want to play again? (Type Y or N) ")
+        if answer == "Y" or answer == "y":
+            guesses = 0
+            guessesLeft = 5
+        elif answer == "N" or answer == "n":
+            print("Thanks for playing!")
+            guessesLeft = 0
+    elif userGuess > numberToGuess and guessesLeft > 0:
+        print("Too high. Try guessing a little lower this time")
+    elif userGuess < numberToGuess and guessesLeft > 0:
+        print("Too low. Aim higher this time")
+    elif userGuess != numberToGuess and guessesLeft == 0:
+        print(f"Too bad, you ran out of turns, the number was {numberToGuess}. Better luck next time.")
