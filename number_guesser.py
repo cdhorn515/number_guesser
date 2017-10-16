5710b234e7baad8b0f0545eedb148723fce7a8c2
Christina Cornell <cdhorn515@gmail.com>
2017-10-16 14:02:28 -0400

    added function to ask if user wants to play again

--- number_guesser.py
+++ number_guesser.py
@@ -5,7 +5,7 @@ def main():
     guessesLeft = 5
     guesses = 0
 
-    numberToGuess = random.randint(1, 101)
+    numberToGuess = random.randint(1, 100)
 
     while guessesLeft > 0:
         userGuess = int(input("Please enter a number between 1 and 100: "))
@@ -14,19 +14,24 @@ def main():
 
         if userGuess == numberToGuess:
             print(f"Hooray! You guessed the number in {guesses} guesses!")
-            answer = input("Do you want to play again? (Type Y or N) ")
-            if answer == "Y" or answer == "y":
-                guesses = 0
-                guessesLeft = 5
-            elif answer == "N" or answer == "n":
-                print("Thanks for playing!")
-                guessesLeft = 0
+            ask_to_play_again()
         elif userGuess > numberToGuess and guessesLeft > 0:
             print("Too high. Try guessing a little lower this time")
         elif userGuess < numberToGuess and guessesLeft > 0:
             print("Too low. Aim higher this time")
         elif userGuess != numberToGuess and guessesLeft == 0:
             print(f"Too bad, you ran out of turns, the number was {numberToGuess}. Better luck next time.")
+            ask_to_play_again()
+
+def ask_to_play_again():
+    answer = input("Do you want to play again? (Type Y or N) ")
+    if answer == "Y" or answer == "y":
+        # guesses = 0
+        # guessesLeft = 5
+        main()
+    elif answer == "N" or answer == "n":
+        print("Thanks for playing!")
+        guessesLeft = 0
 
 if __name__ == '__main__':
     main()
